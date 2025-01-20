5e8ad26adafdb90569994c341b5b716eb2502460
frodtor1209 <frodtor427@iesalmudeyne.es>
2025-01-20 13:04:12 +0100

    Subido estado de componente - Fran

---
+++ src/components/Contador.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const Contador = () => {
+    const [contador, setContador] = useState(0);
+
+    const incrementar = () => setContador(contador + 1);
+    const decrementar = () => setContador(contador - 1);
+    const reiniciar = () => setContador(0);
+
+    return (
+        <>
+            <p className="text-lg text-gray-700">El contador actual es:</p>
+            <div className="text-4xl font-bold text-gray-800 my-4">{contador}</div>
+            <div className="space-x-4">
+                <button
+                    onClick={decrementar}
+                    className="px-4 py-2 bg-red-500 text-white rounded-md shadow hover:bg-red-600 transition"
+                >
+                    Decrementar
+                </button>
+                <button
+                    onClick={incrementar}
+                    className="px-4 py-2 bg-green-500 text-white rounded-md shadow hover:bg-green-600 transition"
+                >
+                    Incrementar
+                </button>
+                <button
+                    onClick={reiniciar}
+                    className="px-4 py-2 bg-orange-500 text-white rounded-md shadow hover:bg-green-600 transition"
+                >
+                    Reiniciar
+                </button>
+            </div>
+        </>
+    );
+};
+
+export default Contador;
--- src/pages/Francisco.jsx
+++ src/pages/Francisco.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import Card from '../components/Card';
+import Contador from '../components/Contador';
+
 
 const Francisco = () => {
-  return (
-    <div className="p-6">
-      <h1 className="text-4xl font-bold">Página de Francisco</h1>
-    </div>
-  );
+    return (
+        <div className="p-6">
+            <h1 className="text-4xl font-bold">Página de Francisco Javier</h1>
+            <div className="bg-white shadow-lg rounded-lg p-6 hover:shadow-2xl transition m-12 border-gray-200 border-2">
+                <h2 className="text-2xl font-bold mb-2 text-gray-800">1. Componentes</h2>
+                <Card
+                    nombre={"Nombre de prueba"}
+                    descripcion={"lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"}
+                    img={"prueba.jpg"}
+                />
+            </div>
+            <div className="bg-white shadow-lg rounded-lg p-6 hover:shadow-2xl transition m-12 border-gray-200 border-2">
+                <h2 className="text-2xl font-bold mb-2 text-gray-800">2. Estado de los componentes</h2>
+                <Contador/>
+            </div>
+        </div>
+    );
 };
 
 export default Francisco;
