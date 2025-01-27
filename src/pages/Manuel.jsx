ccf7d6a41825ea9c0fed4d41e70666a2cf4e0cd1
mangompae <mgomezpaez280705@gmail.com>
2025-01-27 12:16:27 +0100

    formulario subido

--- src/pages/Manuel.jsx
+++ src/pages/Manuel.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import Formulario from '../components/Formulario';
+import Tabla from '../components/tabla';
+import Manuel from './pages/Manuel';
+
 
 const Manuel = () => {
   return (
     <div className="p-6">
       <h1 className="text-4xl font-bold">Página de Manuel</h1>
       <p>Contenido específico de Manuel.</p>
+
+      <Tabla />
+      <Formulario/>
     </div>
   );
 };
